0abc8cfad150abd0986b0c3ac888cf20a0042d0c
Anupa Prabhasara <info@anupa.lk>
2025-08-26 01:25:16 +0530

    booking page

--- app/src/main/java/com/anupa/railease/SearchResult.kt
+++ app/src/main/java/com/anupa/railease/SearchResult.kt
@@ -10,7 +10,7 @@ class SearchResult : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_search_result)
+        setContentView(R.layout.search_result)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
