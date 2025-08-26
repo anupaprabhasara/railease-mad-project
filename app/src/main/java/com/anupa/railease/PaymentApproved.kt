d5cfac68d2371a47117738d8662fb99b48e0e0f7
Anupa Prabhasara <info@anupa.lk>
2025-08-27 01:16:05 +0530

    e ticket print

--- app/src/main/java/com/anupa/railease/PaymentApproved.kt
+++ app/src/main/java/com/anupa/railease/PaymentApproved.kt
@@ -1,6 +1,9 @@
 package com.anupa.railease
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,36 @@ class PaymentApproved : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val ivBack = findViewById<ImageView>(R.id.ivBack)
+        ivBack.setOnClickListener {
+            startActivity(Intent(this, HomePage::class.java))
+        }
+
+        val homePage = findViewById<ImageView>(R.id.homePage)
+        homePage.setOnClickListener {
+            startActivity(Intent(this, HomePage::class.java))
+        }
+
+        val bookingPage = findViewById<ImageView>(R.id.bookingPage)
+        bookingPage.setOnClickListener {
+            startActivity(Intent(this, BookingPage::class.java))
+        }
+
+        val ticketsPage = findViewById<ImageView>(R.id.ticketsPage)
+        ticketsPage.setOnClickListener {
+            startActivity(Intent(this, MyETickets::class.java))
+        }
+
+        val profilePage = findViewById<ImageView>(R.id.profilePage)
+        profilePage.setOnClickListener {
+            startActivity(Intent(this, MyProfile::class.java))
+        }
+
+        val viewETicket = findViewById<Button>(R.id.viewETicket)
+        viewETicket.setOnClickListener {
+            startActivity(Intent(this, ETicketPrint::class.java))
         }
     }
 }
